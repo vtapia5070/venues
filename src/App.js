2e2ef587417718b7ac04e25d1e4549fdf3ef8a8e
Victoria <tapia.victoria5070@gmail.com>
2019-01-05 14:26:46 -0800

    add .env to gitignore, get users location

--- src/App.js
+++ src/App.js
@@ -3,6 +3,44 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor () {
+    super();
+
+    this.state = {
+      userLocation: {
+        longitude: '',
+        latitude: '',
+      }
+    }
+  }
+
+  componentWillMount () {
+    this.getUserLocation();
+  }
+
+  getUserLocation () {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const userLocation = {
+          longitude: position.coords.longitude,
+          latitude: position.coords.latitude
+        };
+        this.setState({
+          ...this.state,
+          userLocation,
+        })
+      }, (error) => {
+        // TODO: handle error and notify user.
+        console.error('An error has occured while retrieving location', error);
+      });
+    } else {
+      // TODO: render modal that prevents user from accessing app
+      // and notifies that the location is needed.
+      // Perhaps render an input that will allow the user to ender their city and state
+      console.log('geolocation is not enabled on this browser');
+    }
+  }
+
   render() {
     return (
       <div className="App">
