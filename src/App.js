dcae2fccbebc51a2a82638197a9e65b0120b488f
Victoria <tapia.victoria5070@gmail.com>
2019-01-06 21:04:18 -0800

    add button and input component to retrieve user's location

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import UserLocation from './UserLocation/UserLocation.js';
 import './App.css';
 
 class App extends Component {
@@ -10,54 +10,29 @@ class App extends Component {
       userLocation: {
         longitude: '',
         latitude: '',
+        city: '',
+        state: '',
       }
     }
   }
 
-  componentWillMount () {
-    this.getUserLocation();
-  }
-
-  getUserLocation () {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const userLocation = {
-          longitude: position.coords.longitude,
-          latitude: position.coords.latitude
-        };
-        this.setState({
-          ...this.state,
-          userLocation,
-        })
-      }, (error) => {
-        // TODO: handle error and notify user.
-        console.error('An error has occured while retrieving location', error);
-      });
-    } else {
-      // TODO: render modal that prevents user from accessing app
-      // and notifies that the location is needed.
-      // Perhaps render an input that will allow the user to ender their city and state
-      console.log('geolocation is not enabled on this browser');
-    }
+  handleLocationChange = (locationValues) => {
+    this.setState({
+      ...this.state,
+      userLocation: locationValues,
+    });
+    
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <UserLocation storeLocation={this.handleLocationChange} />
+        {/* {
+          this.state.userLocation.longitude || this.state.userLocation.city && (
+            
+          )
+        } */}
       </div>
     );
   }
---
+++ src/UserLocation/UserLocation.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+class UserLocation extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      longitude: '',
+      latitude: '',
+      city: '',
+      state: '',
+    }
+  }
+
+  getGeoLocation = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+
+        const locationObj = {
+          longitude: position.coords.longitude,
+          latitude: position.coords.latitude
+        }
+
+        this.setState({
+          ...this.state,
+          ...locationObj,
+        });
+        
+        this.props.storeLocation(this.state);
+        
+      }, (error) => {
+        // TODO: handle error and notify user.
+        console.error('An error has occured while retrieving location', error);
+      });
+    } else {
+      // TODO: render modal that prevents user from accessing app
+      // and notifies that the location is needed.
+      // Perhaps render an input that will allow the user to ender their city and state
+      console.log('geolocation is not enabled on this browser');
+    }
+  }
+
+  handleChange = (name) => (event) => {
+    this.setState({
+      ...this.state,
+      [name]: event.target.value
+    });
+    console.log('handle change', name, event.target.value);
+  }
+
+  render() {
+
+    return (
+      <section>
+        <div className="findCurrentLocation">
+          <Button
+            variant="outlined"
+            color="primary"
+            onClick={this.getGeoLocation}
+          >
+          Find My Current Location!
+          </Button>
+        </div>
+        <div className="locationInput">
+          <TextField
+            name="city"
+            label="City"
+            value={this.state.city}
+            onChange={this.handleChange('city')}
+            margin="normal"
+          />
+          <TextField
+            name="state"
+            label="State"
+            value={this.state.state}
+            onChange={this.handleChange('state')}
+            margin="normal"
+          />
+          <Button 
+            onClick={() => this.props.storeLocation(this.state)}
+            variant="outlined" 
+            color="secondary"
+            disabled={!this.state.city && !this.state.state}
+          >
+            Continue
+          </Button>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default UserLocation;
